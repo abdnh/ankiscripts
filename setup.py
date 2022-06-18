f046024adf4ca755f6026e210c9fbdee0bd1b3ce
Abdo <abd.nh25@gmail.com>
2022-06-18 06:54:15 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="ankibuild",
-    version="0.0.1",
+    version="0.1.0",
     description="Simple script to build Anki add-ons",
     author="Abdo",
     author_email="abd.nh25@gmail.com",
