391741e82f7b66b6b5361221b3bd308963bc066a
Abdo <abd.nh25@gmail.com>
2022-06-20 08:23:46 +0300

    Add classifiers arg to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     author="Abdo",
     author_email="abd.nh25@gmail.com",
     url="https://github.com/abdnh/ankibuild",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
     py_modules=["ankibuild", "ankirun"],
     extras_require={
         "qt5": ["pyqt5"],
