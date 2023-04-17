3740fcfa49fb97c9f8ca079b2169a93f7bc6829a
Abdo <abdo@abdnh.net>
2023-04-17 20:31:18 +0300

    Allow setting support links in init script

--- src/ankiscripts/init.py
+++ src/ankiscripts/init.py
@@ -12,6 +12,8 @@ import types
 import venv
 from pathlib import Path
 
+from . import support
+
 addon_root = Path(".")
 
 parser = argparse.ArgumentParser()
@@ -36,14 +38,35 @@ parser.add_argument(
     "--homepage",
     help="add-on homepage",
     required=False,
+    default="",
 )
 parser.add_argument(
     "--min_point_version",
     help="minimum supported version",
     required=False,
 )
+parser.add_argument(
+    "--github",
+    help="link to github issues",
+    required=False,
+)
+parser.add_argument(
+    "--forums",
+    help="link to the add-on page in Anki forums",
+    required=False,
+)
 
 args = parser.parse_args()
+
+support_links = {}
+if args.github:
+    github_issues = args.github
+elif args.homepage:
+    github_issues = args.homepage
+if github_issues:
+    support_links["GITHUB_ISSUES"] = github_issues
+if args.forums:
+    support_links["FORUMS_PAGE"] = args.forums
 
 # addon.json
 addon_json_path = addon_root / "addon.json"
@@ -71,7 +94,13 @@ readme = re.sub(
     readme,
     flags=re.MULTILINE | re.DOTALL,
 )
+readme = support.format(readme, support_links)
 readme_path.write_text(readme, encoding="utf-8")
+
+ankiweb_page_path = addon_root / "ankiweb_page.html"
+ankiweb_readme = ankiweb_page_path.read_text(encoding="utf-8")
+ankiweb_readme = support.format(ankiweb_readme, support_links)
+ankiweb_page_path.write_text(ankiweb_readme, encoding="utf-8")
 
 # Symlinking
 src_path = addon_root / "src"
--- src/ankiscripts/support.py
+++ src/ankiscripts/support.py
@@ -6,17 +6,20 @@ python -m ankiscripts.support --type github GITHUB_ISSUES https://github.com/abd
 from argparse import ArgumentParser
 from pathlib import Path
 
+DEFAULTS = {
+    "GITHUB_ISSUES": "https://github.com/abdnh/anki-addon-template/issues",
+    "FORUMS_PAGE": "https://forums.ankiweb.net/c/add-ons/11",
+}
 
-def format(type: str, fmt_args: dict[str, str]) -> str:
-    fmt_args.setdefault(
-        "GITHUB_ISSUES", "https://github.com/abdnh/anki-addon-template/issues"
-    )
-    fmt_args.setdefault("FORUMS_PAGE", "https://forums.ankiweb.net/c/add-ons/11")
-    ext = ".md" if type == "github" else ".html"
-    file = Path(__file__).parent / f"support{ext}"
-    with open(file, "r", encoding="utf-8") as file:
-        text = file.read().format(**fmt_args)
-        return text
+
+def format(text: str, fmt_args: dict[str, str]) -> str:
+    copy = DEFAULTS.copy()
+    copy.update(fmt_args)
+    fmt_args = copy
+    for key, value in DEFAULTS.items():
+        text = text.replace(value, fmt_args[key])
+    text = text.format(**fmt_args)
+    return text
 
 
 if __name__ == "__main__":
@@ -30,4 +33,8 @@ if __name__ == "__main__":
     )
     args, unknown = parser.parse_known_args()
     fmt_args = dict((unknown[i], unknown[i + 1]) for i in range(0, len(unknown), 2))
-    print(format(args.type, fmt_args))
+    ext = ".md" if args.type == "github" else ".html"
+    file = Path(__file__).parent / f"support{ext}"
+    with open(file, "r", encoding="utf-8") as file:
+        text = file.read()
+        print(format(text, fmt_args))
