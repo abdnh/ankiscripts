efbd38adb38a5b306fa4b04def14aff2d479434d
Abdo <abdo@abdnh.net>
2023-08-12 23:34:29 +0300

    Run custom scripts/vendor.sh in vendor.py

--- src/ankiscripts/vendor.py
+++ src/ankiscripts/vendor.py
@@ -1,4 +1,5 @@
 import shutil
+import subprocess
 import sys
 from pathlib import Path
 
@@ -16,10 +17,16 @@ def install_libs() -> None:
         return
     vendor_path = addon_root / "src" / "vendor"
     bin_path = vendor_path / "bin"
+    # Additional vendoring logic (e.g. installing node modules) can be specified in scripts/vendor.sh
+    vendor_script_path = addon_root / "scripts" / "vendor.sh"
     python_exe = shutil.which("python")
+    bash_exe = shutil.which("bash")
 
     pip_install(python_exe, str(reqs_path), str(vendor_path))
     shutil.rmtree(bin_path)
+    if vendor_script_path.exists():
+        # Seems like Bash on Windows expects POSIX paths
+        subprocess.check_call([bash_exe, str(vendor_script_path.as_posix())])
 
 
 if __name__ == "__main__":
