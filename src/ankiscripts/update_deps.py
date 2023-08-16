2c67ca79483e764c6425511fd7480c95c26ec0c5
Abdo <abdo@abdnh.net>
2023-08-16 21:37:40 +0300

    Avoid running pip-compile on empty files
    Because it adds an extra newline, which makes the end-of-file-fixer
    pre-commit hook complain.

--- src/ankiscripts/update_deps.py
+++ src/ankiscripts/update_deps.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import os
 import shutil
 import subprocess
 import sys
@@ -8,15 +9,26 @@ from pathlib import Path
 
 from ._utils import add_exe_suffix, pip_install
 
+pip_compile_args = [
+    "--resolver=backtracking",
+    "--allow-unsafe",
+    "--no-header",
+    "--strip-extras",
+    "--upgrade",
+]
+
+
+def compile_requirements(pip_compile_exe: str, req_type: str) -> None:
+    requirements_filename = f"requirements/{req_type}.in"
+    if os.path.exists(requirements_filename):
+        with open(requirements_filename, "r", encoding="utf-8") as file:
+            if file.read().strip():
+                subprocess.check_call(
+                    [pip_compile_exe, *pip_compile_args, requirements_filename]
+                )
+
 
 def update_deps(bin_path: str | Path, req_type: str | None = None) -> None:
-    args = [
-        "--resolver=backtracking",
-        "--allow-unsafe",
-        "--no-header",
-        "--strip-extras",
-        "--upgrade",
-    ]
     bin_path = Path(bin_path)
     if bin_path.exists():
         python_exe = add_exe_suffix(str(bin_path / "python"))
@@ -27,12 +39,12 @@ def update_deps(bin_path: str | Path, req_type: str | None = None) -> None:
         pip_compile_exe = shutil.which("pip-compile")
         pip_sync_exe = shutil.which("pip-sync")
     if req_type is not None:
-        subprocess.check_call([pip_compile_exe, *args, f"requirements/{req_type}.in"])
+        compile_requirements(pip_compile_exe, req_type)
     else:
         pip_install(python_exe, "requirements/base.txt")
-        subprocess.check_call([pip_compile_exe, *args, "requirements/base.in"])
-        subprocess.check_call([pip_compile_exe, *args, "requirements/bundle.in"])
-        subprocess.check_call([pip_compile_exe, *args, "requirements/dev.in"])
+        compile_requirements(pip_compile_exe, "base")
+        compile_requirements(pip_compile_exe, "bundle")
+        compile_requirements(pip_compile_exe, "dev")
         subprocess.check_call([pip_sync_exe, "requirements/dev.txt"])
 
 
