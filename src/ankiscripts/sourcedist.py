fc1a12d4a0486799bf5de892db4d520e6b98fddf
Abdo <abdo@abdnh.net>
2024-04-20 21:50:17 +0300

    Bundle all branches

--- src/ankiscripts/sourcedist.py
+++ src/ankiscripts/sourcedist.py
@@ -15,9 +15,7 @@ with tempfile.TemporaryDirectory() as tempdir:
     zip_path = build_dir / (package + "_sources.zip")
     bundle_path = Path(tempdir) / "repo.bundle"
     archive_path = Path(tempdir) / "repo.zip"
-    subprocess.run(
-        [git_exe, "bundle", "create", str(bundle_path), "master"], check=True
-    )
+    subprocess.run([git_exe, "bundle", "create", str(bundle_path), "--all"], check=True)
     subprocess.run([git_exe, "archive", "HEAD", "-o", str(archive_path)], check=True)
     with zipfile.ZipFile(zip_path, "w") as file:
         file.write(bundle_path, bundle_path.name)
