5aee2443798ca811ee561c84988edde633ac654f
Abdo <abdo@abdnh.net>
2026-01-26 03:50:30 +0300

    Update init script to use Copier template

--- src/ankiscripts/init.py
+++ src/ankiscripts/init.py
@@ -3,12 +3,13 @@ Initializes a new add-on using my add-on template.
 """
 
 import argparse
-import re
 import shutil
 from pathlib import Path
 
-from . import support, vendor
-from ._utils import read_addon_json, symlink_addon, uv, write_addon_json
+import copier
+
+from . import vendor
+from ._utils import symlink_addon, uv
 
 
 def replace_in_path(path: Path, old: str, new: str) -> None:
@@ -19,117 +20,26 @@ def replace_in_path(path: Path, old: str, new: str) -> None:
     path.write_text(text, encoding=encoding)
 
 
-addon_root = Path(".")
+def main() -> None:
+    parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--destination", help="folder to initialize add-on in", default="."
+    )
+    args = parser.parse_args()
+    addon_root = Path(args.destination)
+    copier.run_copy(
+        "https://github.com/abdnh/anki-addon-template", data=None, unsafe=True
+    )
 
-parser.add_argument(
-    "--name",
-    help="add-on name",
-    required=True,
-)
-parser.add_argument(
-    "--package",
-    help="add-on package",
-    required=True,
-)
+    uv("sync", "--dev")
+    uv("run", "--", "prek", "install", "--install-hooks")
+    vendor.install_libs()
 
-parser.add_argument(
-    "--ankiweb_id",
-    help="AnkiWeb ID",
-    required=False,
-)
-parser.add_argument(
-    "--homepage",
-    help="add-on homepage",
-    required=False,
-    default="",
-)
-parser.add_argument(
-    "--min_point_version",
-    help="minimum supported version",
-    required=False,
-)
-parser.add_argument(
-    "--github",
-    help="link to github issues",
-    required=False,
-)
-parser.add_argument(
-    "--forums",
-    help="link to the add-on page in Anki forums",
-    required=False,
-)
+    shutil.copytree(addon_root / ".vscode.dist", addon_root / ".vscode")
 
-args = parser.parse_args()
+    symlink_addon(addon_root, args.package)
 
-support_links = {}
-github_issues = None
-if args.github:
-    github_issues = args.github
-elif args.homepage:
-    github_issues = args.homepage
-if github_issues:
-    support_links["GITHUB_ISSUES"] = github_issues
-if args.forums:
-    support_links["FORUMS_PAGE"] = args.forums
 
-# addon.json
-addon_meta = read_addon_json(addon_root)
-addon_meta["name"] = args.name
-addon_meta["package"] = args.package
-if args.ankiweb_id:
-    addon_meta["ankiweb_id"] = args.ankiweb_id
-elif "ankiweb_id" in addon_meta:
-    del addon_meta["ankiweb_id"]
-if args.homepage:
-    addon_meta["homepage"] = args.homepage
-if args.min_point_version:
-    addon_meta["min_point_version"] = args.min_point_version
-support_channels = {}
-if args.github:
-    support_channels["github"] = args.github
-if args.forums:
-    support_channels["forums"] = args.forums
-addon_meta["support_channels"] = support_channels
-write_addon_json(addon_root, addon_meta)
-
-replace_in_path(addon_root / "pyproject.toml", "anki_addon_template", args.package)
-
-# Readme
-readme_path = addon_root / "README.md"
-readme = readme_path.read_text(encoding="utf-8")
-readme = re.sub(
-    r".*?\[BEGINNING OF TEMPLATE\]",
-    f"# {args.name}",
-    readme,
-    flags=re.MULTILINE | re.DOTALL,
-)
-readme = support.format(readme, support_links)
-readme_path.write_text(readme, encoding="utf-8")
-
-ankiweb_page_path = addon_root / "ankiweb_page.html"
-ankiweb_readme = ankiweb_page_path.read_text(encoding="utf-8")
-ankiweb_readme = support.format(ankiweb_readme, support_links)
-ankiweb_page_path.write_text(ankiweb_readme, encoding="utf-8")
-
-# Symlinking
-symlink_addon(addon_root, args.package)
-
-# Set up virtual environment and install dependencies
-uv("sync")
-uv("run", "--", "prek", "install", "--install-hooks")
-vendor.install_libs()
-
-# Copy VS Code settings
-vscode_dist = addon_root / ".vscode.dist"
-launch_config_path = vscode_dist / "launch.json"
-replace_in_path(vscode_dist / "launch.json", "ADDON_NAME", args.package.upper())
-vscode_path = addon_root / ".vscode"
-shutil.copytree(vscode_dist, vscode_path)
-
-# Update book title
-replace_in_path(addon_root / "docs" / "book.toml", "ADDON_NAME", args.package.upper())
-
-# Update package name in package.json
-replace_in_path(addon_root / "ts" / "package.json", "my_addon", args.package)
+if __name__ == "__main__":
+    main()
